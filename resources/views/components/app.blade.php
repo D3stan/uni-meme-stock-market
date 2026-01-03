ce5991393eb4940e330ed343e6629a2358f5931e
TodeschiMatteo <matteo.todeschi2@studio.unibo.it>
2026-01-03 14:28:54 +0100

    Updated app.blade.php to use base.blade.php

--- resources/views/components/app.blade.php
+++ resources/views/components/app.blade.php
@@ -1,16 +1,4 @@
-<!DOCTYPE html>
-@props(['active' => null, 'balance' => null])
-<html lang="it" class="dark">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>{{ $title ?? 'AlmaStreet' }}</title>
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
-    <link rel="stylesheet" href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600,700,800">
-    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
-</head>
-<body class="bg-background text-white font-sans antialiased">
-
+<x-base>
     {{-- Navigation bar component (renders fixed navs) --}}
     <x-navigation.navigation-bar :active="$active ?? null" :balance="$balance ?? null" />
 
@@ -20,5 +8,4 @@
     <main>
         {{ $slot }}
     </main>
-</body>
-</html>
+</x-base>
