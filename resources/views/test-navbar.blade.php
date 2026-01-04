82121f304cceccb4fac75f0facf8c3877840a1b0
TodeschiMatteo <matteo.todeschi2@studio.unibo.it>
2026-01-04 13:48:05 +0100

    refactor

--- resources/views/test-navbar.blade.php
+++ resources/views/test-navbar.blade.php
@@ -1,15 +1,10 @@
-<x-app :active="'create'" :balance="'1,250.00'">
-    <div class="pb-20 lg:pb-8 lg:pt-20">
-        <div class="max-w-4xl mx-auto px-4 py-8 space-y-12">
-            
-            {{-- Header --}}
-            <div class="text-center space-y-4">
-                <h1 class="text-4xl font-black text-green-500">Navigation Bar Test</h1>
-                <div class="inline-flex items-center gap-2 px-4 py-2 bg-gray-800 rounded-lg text-sm">
-                    <span class="material-icons text-green-500 text-base">info</span>
-                    <span>Ridimensiona la finestra per vedere la versione desktop</span>
-                </div>
-            </div>
+<x-app :active="'create'" :balance="'1,250.00'">        
+    {{-- Header --}}
+    <div class="text-center space-y-4">
+        <h1 class="text-4xl font-black text-green-500">Navigation Bar Test</h1>
+        <div class="inline-flex items-center gap-2 px-4 py-2 bg-gray-800 rounded-lg text-sm">
+            <span class="material-icons text-green-500 text-base">info</span>
+            <span>Ridimensiona la finestra per vedere la versione desktop</span>
         </div>
     </div>
 </x-app>
