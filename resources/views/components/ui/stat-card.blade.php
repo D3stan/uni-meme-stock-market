75f10b0c471fb8b580d374f00cba645d6b6e5590
TodeschiMatteo <matteo.todeschi2@studio.unibo.it>
2026-01-04 15:53:56 +0100

    Added standard table component

--- resources/views/components/ui/stat-card.blade.php
+++ resources/views/components/ui/stat-card.blade.php
@@ -4,7 +4,7 @@
     'color' => 'white',
 ])
 
-<div class="bg-gray-900/50 rounded-xl p-4 border border-gray-800">
+<div class="bg-gray-900 rounded-xl p-4 border border-gray-800">
     <p class="text-gray-400 text-sm mb-1">{{ $title }}</p>
     <p class="text-2xl font-bold text-{{ $color }}">{{ $value }}</p>
 </div>
