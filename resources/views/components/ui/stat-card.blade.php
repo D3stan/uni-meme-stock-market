877544d660a27c73cd8777ef256ff7de849fc77c
TodeschiMatteo <matteo.todeschi2@studio.unibo.it>
2026-01-04 14:41:31 +0100

    Created component stat-card

---
+++ resources/views/components/ui/stat-card.blade.php
@@ -0,0 +1,10 @@
+@props([
+    'title' => '',
+    'value' => '',
+    'color' => 'white',
+])
+
+<div class="bg-gray-900/50 rounded-xl p-4 border border-gray-800">
+    <p class="text-gray-400 text-sm mb-1">{{ $title }}</p>
+    <p class="text-2xl font-bold text-{{ $color }}">{{ $value }}</p>
+</div>
