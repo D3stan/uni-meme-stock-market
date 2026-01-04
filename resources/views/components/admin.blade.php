e4056bf83326411f3fbd866a9fc31a6b2d8c671a
TodeschiMatteo <matteo.todeschi2@studio.unibo.it>
2026-01-04 16:13:57 +0100

    Base layout refactor

--- resources/views/components/admin.blade.php
+++ resources/views/components/admin.blade.php
@@ -5,7 +5,7 @@
     'href' => null,
 ])
 
-<x-base>
+<x-base title="Admin">
     <div class="min-h-screen pb-8 pt-6">
         <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
             {{-- Header --}}
